711431af5adb4e1d8ca83a3357c1532fd9856907
Sirix <sirixb@gmail.com>
2019-01-27 18:22:16 -0500

    Roll a ball V1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+//Behaviour= comportamiento
+public class CameraController : MonoBehaviour 
+{
+	//aca declaro una variable publica donde guardo las propiedades del jugador
+	public GameObject player;
+	// variable de desplazamiento
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position;
+		//offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	// y cambio update por LateUpdate que es mejor para seguir y rercolectar los datos de las camaras
+	void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;//unity 5
+using System.Collections;
+//algunas cosas que estan comentadas son de la actualizacion de unity 5 en el projecto roll a ball.
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public GUIText countText;
+	//public Text countTex; //este lo realizaron con un Ui canvas text.
+	public GUIText winText;
+	//public Text winText; //este lo realizaron con un Ui canvas text.
+	private int count;
+	//private Rigidbody rb;
+
+	void Start()
+	{
+		//rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	void FixedUpdate()
+		{
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+
+			Vector3 movement = new Vector3(moveHorizontal,0.0f, moveVertical);
+			
+			rigidbody.AddForce(movement*speed*Time.deltaTime);
+			//rb.AddForce (movement * speed);
+		}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp") 
+		{
+			other.gameObject.SetActive(false);
+			count= count+1; 
+			SetCountText ();
+		}
+		/*if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+		}*/
+	}
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "TU GANAS!";
+		}
+	}
+}
